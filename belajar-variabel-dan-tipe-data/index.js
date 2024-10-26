52085215fbdd3d6db8e20a599af5941c8b022ade
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-26 15:06:25 +0700

    first commit

---
+++ belajar-variabel-dan-tipe-data/index.js
@@ -0,0 +1,21 @@
+/**
+ * TODO:
+ * 1. Buatlah variabel-variabel berikut:
+ *    - fullName: gabungan dari firstName dan lastName, dipisahkan oleh spasi.
+ *    - yearsUntilRetirement: berapa tahun lagi sebelum seseorang pensiun, diasumsikan pensiun pada usia 65 tahun.
+ *    - info: sebuah string yang berisi informasi tentang seseorang, contohnya: "John Doe is 25 years old and is married."
+ */
+
+// Tulis kode di bawah ini
+var firstName = "John";
+var lastName = "Doe";
+var age = 25;
+var isMarried = true;
+
+// Tulis kode tambahan di bawah ini
+
+// Variabel fullName
+
+// Variabel yearsUntilRetirement
+
+// Variabel info
